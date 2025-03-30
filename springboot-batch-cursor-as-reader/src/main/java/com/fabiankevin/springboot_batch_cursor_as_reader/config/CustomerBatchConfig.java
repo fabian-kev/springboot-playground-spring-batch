9b9644aea79a633e12e554bd342bb88f1ebf6423
kev <fbian.kevin@gmail.com>
2025-03-30 17:45:08 +0800

    Add JdbcBatchItemWriter sample

--- springboot-batch-cursor-as-reader/src/main/java/com/fabiankevin/springboot_batch_cursor_as_reader/config/CustomerBatchConfig.java
+++ springboot-batch-cursor-as-reader/src/main/java/com/fabiankevin/springboot_batch_cursor_as_reader/config/CustomerBatchConfig.java
@@ -3,22 +3,21 @@ package com.fabiankevin.springboot_batch_cursor_as_reader.config;
 import com.fabiankevin.springboot_batch_cursor_as_reader.persistence.CustomerEntity;
 import com.fabiankevin.springboot_batch_cursor_as_reader.persistence.CustomerRepository;
 import com.zaxxer.hikari.HikariDataSource;
-import jakarta.persistence.EntityManagerFactory;
 import lombok.extern.slf4j.Slf4j;
-import org.hibernate.SessionFactory;
-import org.hibernate.StatelessSession;
-import org.hibernate.Transaction;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
-import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
 import org.springframework.batch.item.database.JdbcCursorItemReader;
+import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
 import org.springframework.batch.item.database.builder.JdbcCursorItemReaderBuilder;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.transaction.PlatformTransactionManager;
 
 import java.util.stream.Collectors;
@@ -41,55 +40,43 @@ public class CustomerBatchConfig {
     }
 
     @Bean
-    public ItemProcessor<CustomerEntity, CustomerEntity> customerEntityItemProcessor() {
-        return item -> {
-            item.setStatus("ACTIVE");
-            return item;
+    public ItemWriter<CustomerEntity> customersUpdateWriter(CustomerRepository customerRepository) {
+        return items -> {
+            customerRepository.updateAllToActiveById(items.getItems().stream()
+                    .map(CustomerEntity::getId).collect(Collectors.toSet()));
         };
     }
 
     @Bean
-    public ItemWriter<CustomerEntity> jpaCustomerWriter(CustomerRepository customerRepository) {
-        return chunk -> {
-            customerRepository.updateAllToActiveById(chunk.getItems().stream()
-                    .map(CustomerEntity::getId)
-                    .collect(Collectors.toSet()));
-            log.info("{} have been saved.", chunk.getItems().size());
-        };
-    }
-
-    //StatelessSession Limitation:
-    //StatelessSession doesn’t use the persistence context, so features like dirty checking or cascading don’t apply. This is fine for your use case (simple updates), but it’s worth noting.
-    @Bean("statelessItemWriter")
-    public ItemWriter<CustomerEntity> statelessItemWriter(EntityManagerFactory entityManagerFactory) {
-        return items -> {
-            StatelessSession session = entityManagerFactory.unwrap(SessionFactory.class).openStatelessSession();
-            Transaction tx = session.beginTransaction();
-            try {
-                for (CustomerEntity item : items) {
-                    session.update(item);
-                }
-                tx.commit();
-            } catch (Exception e) {
-                tx.rollback();
-                throw e;
-            } finally {
-                session.close();
-            }
-        };
+    public JdbcBatchItemWriter<CustomerEntity> jdbcBatchItemWriter(HikariDataSource hikariDataSource) {
+        String insertSql = """
+                INSERT INTO customers(id, name, status, created_at)
+                VALUES(:id, :name, :status, :created_at)
+                """;
+        return new JdbcBatchItemWriterBuilder<CustomerEntity>()
+                .dataSource(hikariDataSource)
+                .sql(insertSql)
+                .itemSqlParameterSourceProvider(item -> {
+                    MapSqlParameterSource params = new MapSqlParameterSource();
+                    params.addValue("id", item.getId());
+                    params.addValue("name", item.getName());
+                    params.addValue("status", item.getStatus());
+                    params.addValue("created_at", item.getCreatedAt());
+                    return params;
+                })
+                .build();
     }
 
     @Bean
     public Step customerStep(JobRepository jobRepository,
                              PlatformTransactionManager transactionManager,
                              JdbcCursorItemReader<CustomerEntity> customerReader,
-                             ItemProcessor<CustomerEntity, CustomerEntity> customerEntityItemProcessor,
-                             ItemWriter<CustomerEntity> jpaCustomerWriter) {
+                             @Qualifier("customersUpdateWriter")
+                             ItemWriter<CustomerEntity> itemWriter) {
         return new StepBuilder("customerStep", jobRepository)
                 .<CustomerEntity, CustomerEntity>chunk(500, transactionManager)
                 .reader(customerReader)
-                .processor(customerEntityItemProcessor)
-                .writer(jpaCustomerWriter)
+                .writer(itemWriter)
                 .build();
     }
 
